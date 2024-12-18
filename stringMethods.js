5e3b028d4247c33b75e38b1b87b27ab5db2eb847
Muhammet Burak Altun <altunburak88@gmail.com>
2024-12-18 16:12:07 +0300

    again string

--- stringMethods.js
+++ stringMethods.js
@@ -1,3 +1,17 @@
+const message = "merhaba " + "js"; // temel birleştirme
+console.log (message); 
+
+const sayi = 12;
+const sayiBirlestir = "merhaba " + sayi + " js"  // string ve number birleştirme number değeri de string oldu 
+console.log(sayiBirlestir);
+console.log(typeof(sayiBirlestir)); // tipini kontrol ettik typeof.js dosyasında örenkler var
+
+console.log("js " + 44); // çıktı "js 44" string ifade
+console.log(true + " js"); // çıktı "true js" string ifade
+console.log(1+2+"4"); //çıktı "34" string ifade işlem önceliği yaptı 
+console.log("3"+1+2); //çıktı "312" string ifade işlem önceliği yaptı string önde olduğu için hepsi string oldu
+
+
 const str1 = "Merhaba";
 const str2 = "Dünya";
 console.log(str1 + " " + str2); //temel birleştirme
